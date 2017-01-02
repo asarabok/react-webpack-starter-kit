aef9f3dbaccaa27c64ea762c501f9e74f0933b5a
asarabok <antoniosarabok@gmail.com>
2017-01-03 00:03:25 +0100

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var SampleComponent = React.createClass({
+
+	render: function()
+	{
+		return ( 
+			<h1 className="header">
+			Hello from samples156
+			</h1>
+		)
+	}
+}); 
+
+export default SampleComponent;
